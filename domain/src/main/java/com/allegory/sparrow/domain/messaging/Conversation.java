bf7a6f2b0a1278ddb4aa1d91ab42905c69d4ca1c
Wesley Brown <wesleybrown@wesleybrown.net>
2020-04-07 22:19:45 -0400

    Remove Unnecessary Classes
    Remove classes that are no longer used such as UndeliveredMessage and
    DeliveredMessages since they have both been replaced by the Message class.

--- domain/src/main/java/com/allegory/sparrow/domain/messaging/Conversation.java
+++ domain/src/main/java/com/allegory/sparrow/domain/messaging/Conversation.java
@@ -1,51 +1,37 @@
 package com.allegory.sparrow.domain.messaging;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
  * A conversation between participants.
  */
 public final class Conversation {
-    private final List<DeliveredMessage> deliveredMessages;
     private final List<Participant> participants;
     private final List<Message> messages;
 
     /**
      * Create a new conversation.
+     *
+     * @param participants the participants of the new conversation.
      */
-    public Conversation() {
-        deliveredMessages = new ArrayList<>();
-        participants = null;
-        messages = new ArrayList<>();
-    }
-
     public Conversation(final List<Participant> participants) {
-        deliveredMessages = null;
         this.participants = participants;
         messages = new ArrayList<>();
-    }
-
-    public List<DeliveredMessage> deliveredMessages() {
-        return Collections.unmodifiableList(deliveredMessages);
     }
 
     public List<Message> messages() {
         return messages;
     }
 
+    /**
+     * Include a message in this conversation.
+     *
+     * @param message the message to include.
+     * @return the included message.
+     */
     public Message includeMessage(final Message message) {
         messages.add(message);
         return message;
-    }
-
-    /**
-     * Include a new delivered message in this conversation.
-     *
-     * @param deliveredMessage the new delivered message to include.
-     */
-    public void includeDeliveredMessage(final DeliveredMessage deliveredMessage) {
-        deliveredMessages.add(deliveredMessage);
     }
 }
--- domain/src/test/java/com/allegory/sparrow/domain/messaging/MessagingTest.java
+++ domain/src/test/java/com/allegory/sparrow/domain/messaging/MessagingTest.java
@@ -1,32 +1,27 @@
 package com.allegory.sparrow.domain.messaging;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 final class MessagingTest {
     private Participant paul;
     private Participant bob;
-    private DeliveredMessage paulsMessage;
+    private Message paulsMessage;
     private Conversation paulAndBobsConversation;
-    private UndeliveredMessage bobsUndeliveredMessage;
 
     @BeforeEach
     void setUp() {
         paul = new Participant("Paul");
         bob = new Participant("Bob");
-        paulsMessage =
-            new DeliveredMessage(paul, bob, "How can I help you?");
-        paulAndBobsConversation = new Conversation();
-        bobsUndeliveredMessage = new UndeliveredMessage(
-            paulAndBobsConversation, bob, "I'm looking to buy a house.");
+        paulsMessage = new Message(paul, bob, "How can I help you?");
+        paulAndBobsConversation = new Conversation(Arrays.asList(paul, bob));
     }
 
     @AfterEach
@@ -35,73 +30,26 @@ final class MessagingTest {
         bob = null;
         paulsMessage = null;
         paulAndBobsConversation = null;
-        bobsUndeliveredMessage = null;
     }
 
     @Test
     void identical_participants_are_unique() {
-        final Participant paul = new Participant("Paul");
         final Participant paulsClone = new Participant("Paul");
         assertNotEquals(paul, paulsClone);
     }
 
     @Test
     void identical_messages_are_unique() {
-        final Participant paul = new Participant("Paul");
-        final Participant bob = new Participant("Bob");
-        final Message paulsMessage = new Message(paul, bob, "How can I help you?");
-        final Message paulsDuplicateMessage = new Message(paul, bob, "How can I help you?");
+        final Message paulsDuplicateMessage =
+            new Message(paul, bob, "How can I help you?");
         assertNotEquals(paulsMessage, paulsDuplicateMessage);
     }
 
     @Test
-    void identical_delivered_messages_have_same_hash_codes() {
-        final DeliveredMessage paulsDuplicateMessage =
-            new DeliveredMessage(paul, bob, "How can I help you?");
-        assertEquals(paulsMessage.hashCode(), paulsDuplicateMessage.hashCode());
-    }
-
-    @Test
-    void identical_delivered_messages_are_equal() {
-        final DeliveredMessage paulsDuplicateMessage =
-            new DeliveredMessage(paul, bob, "How can I help you?");
-        assertEquals(paulsMessage, paulsDuplicateMessage);
-    }
-
-    @Test
     void including_a_message_in_a_conversation_adds_it_to_that_conversations_messages() {
-        final Participant paul = new Participant("Paul");
-        final Participant bob = new Participant("Bob");
-        final Conversation paulAndBobsConversation =
-            new Conversation(Arrays.asList(paul, bob));
-        final Message paulsMessage = new Message(paul, bob, "How can I help you?");
         paulAndBobsConversation.includeMessage(paulsMessage);
-        assertThat(paulAndBobsConversation.messages()).contains(paulsMessage);
-    }
-
-    @Test
-    void including_a_delivered_message_adds_it_to_the_list_of_all_delivered_messages() {
-        paulAndBobsConversation.includeDeliveredMessage(paulsMessage);
-        final List<DeliveredMessage> expectedMessages = new ArrayList<>();
-        expectedMessages.add(paulsMessage);
-        assertThat(paulAndBobsConversation.deliveredMessages())
-            .containsExactlyInAnyOrderElementsOf(expectedMessages);
-    }
-
-    @Test
-    void sending_an_undelivered_message_returns_an_equivalent_delivered_message() {
-        final DeliveredMessage bobsExpectedDeliveredMessage =
-                new DeliveredMessage(bob, paul, "I'm looking to buy a house.");
-        final DeliveredMessage bobsActualDeliveredMessage =
-            bobsUndeliveredMessage.sendTo(paul);
-        assertEquals(bobsExpectedDeliveredMessage, bobsActualDeliveredMessage);
-    }
-
-    @Test
-    void sending_an_undelivered_message_adds_it_to_the_conversation() {
-        final List<DeliveredMessage> expectedMessages = new ArrayList<>();
-        expectedMessages.add(new DeliveredMessage(bob, paul, "I'm looking to buy a house."));
-        bobsUndeliveredMessage.sendTo(paul);
-        assertThat(paulAndBobsConversation.deliveredMessages()).containsExactlyInAnyOrderElementsOf(expectedMessages);
+        final List<Message> actualMessages = paulAndBobsConversation.messages();
+        final List<Message> expectedMessages = Arrays.asList(paulsMessage);
+        assertEquals(expectedMessages, actualMessages);
     }
 }

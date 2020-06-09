4c4f1e6c5db52cab6f045afdd82edb1a0b6226fa
Wesley Brown <wesleybrown@wesleybrown.net>
2020-06-09 16:52:37 -0400

    Ensure Conversations With Same ID Are Equal
    Conversations that have the same ID should be equal since they represents the
    same conversations.

--- domain/src/test/java/com/allegory/sparrow/domain/messaging/When_comparing_conversations.java
+++ domain/src/test/java/com/allegory/sparrow/domain/messaging/When_comparing_conversations.java
@@ -2,23 +2,46 @@ package com.allegory.sparrow.domain.messaging;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 final class When_comparing_conversations
 {
+    private Participant bob;
+    private Conversation bobsConversation;
+    private Conversation bobsDuplicateConversation;
+
+    @BeforeEach
+    void setUp()
+    {
+        bob = Participant.withIdAndName(UUID.randomUUID(), "Bob");
+        bobsConversation = Conversation.withIdBetweenParticipants(
+            UUID.randomUUID(), Arrays.asList(bob));
+        bobsDuplicateConversation = Conversation.withIdBetweenParticipants(
+            bobsConversation.id(), bobsConversation.participants());
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        bob = null;
+        bobsConversation = null;
+        bobsDuplicateConversation = null;
+    }
+
     @Test
     void conversations_with_the_same_id_have_the_same_hash_codes()
     {
-        final Participant bob = Participant.withIdAndName(
-            UUID.randomUUID(), "Bob");
-        final Conversation bobsConversation = Conversation
-            .withIdBetweenParticipants(UUID.randomUUID(), Arrays.asList(bob));
-        final Conversation bobsDuplicateConversation = Conversation
-            .withIdBetweenParticipants(
-                bobsConversation.id(), bobsConversation.participants());
         assertEquals(
             bobsConversation.hashCode(), bobsDuplicateConversation.hashCode());
+    }
+
+    @Test
+    void conversations_with_the_same_id_are_equal()
+    {
+        assertEquals(bobsConversation, bobsDuplicateConversation);
     }
 }

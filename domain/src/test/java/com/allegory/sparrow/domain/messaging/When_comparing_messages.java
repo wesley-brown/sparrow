ccc5e52513b50ba55c78309452783d3c2322021b
Wesley Brown <wesleybrown@wesleybrown.net>
2020-06-09 15:24:27 -0400

    Ensure Messages With Same IDs Are Equal
    Messages with the same IDs should be equal because they represent the same
    message.

--- domain/src/test/java/com/allegory/sparrow/domain/messaging/When_comparing_messages.java
+++ domain/src/test/java/com/allegory/sparrow/domain/messaging/When_comparing_messages.java
@@ -2,21 +2,44 @@ package com.allegory.sparrow.domain.messaging;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.UUID;
 
 final class When_comparing_messages
 {
+    private Participant bob;
+    private Message bobsMessage;
+    private Message bobsDuplicateMessage;
+
+    @BeforeEach
+    void setUp()
+    {
+        bob = Participant.withIdAndName(UUID.randomUUID(), "Bob");
+        bobsMessage = Message.withIdFromSenderWithContent(
+            UUID.randomUUID(), bob, "Hello, World!");
+        bobsDuplicateMessage = Message.withIdFromSenderWithContent(
+            bobsMessage.id(), bobsMessage.sender(), bobsMessage.content());
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        bob = null;
+        bobsMessage = null;
+        bobsDuplicateMessage = null;
+    }
+
     @Test
     void messages_with_the_same_id_have_the_same_hash_codes()
     {
-        final Participant bob = Participant.withIdAndName(
-            UUID.randomUUID(), "Bob");
-        final Message bobsMessage = Message.withIdFromSenderWithContent(
-            UUID.randomUUID(), bob, "Hello, World!");
-        final Message bobsDuplicateMessage =
-            Message.withIdFromSenderWithContent(
-                bobsMessage.id(), bobsMessage.sender(), bobsMessage.content());
         assertEquals(bobsMessage.hashCode(), bobsDuplicateMessage.hashCode());
+    }
+
+    @Test
+    void messages_with_the_same_id_are_equal()
+    {
+        assertEquals(bobsMessage, bobsDuplicateMessage);
     }
 }
